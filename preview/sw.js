fe1087b14dd7be82c34ce7fc5ac82ebb2dca2c80
KKristofH <130987956+KKristofH@users.noreply.github.com>
2026-05-04 00:18:20 +0000

    Deploying to gh-pages from @ KKristofH/bmw-e87-obd2@852752bf534d9a16856793cfb2f8a566412da05f 🚀

---
+++ preview/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'bmw-obd2-v4';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-180.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
